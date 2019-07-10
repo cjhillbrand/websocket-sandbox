6adda0ae8c9cf6296ab277fdc392f08acef23aea
Hillbrand <hillbc@38f9d36425c1.ant.amazon.com>
2019-07-09 18:58:37 -0700

    Fix leave room

--- resources/aws-utils/DisconnectLambda/index.js
+++ resources/aws-utils/DisconnectLambda/index.js
@@ -1,34 +1,26 @@
 var AWS = require('aws-sdk')
 AWS.config.update({region: 'us-east-1'})
-
+// May have to figure out how to delete them from a room 
+// with no info on what room they are in
 exports.handler = async (event) => {
     const db = new AWS.DynamoDB.DocumentClient();
-    const connectionId = event.requestContext.connectionId;
+    const { connectionId } = event.requestContext;
     var params = {
         TableName: "client-records",
         Key: {"ID": connectionId}
     };
     var response = {
         statusCode: 200,
-        body: {
-            deleteOne: null,
-            deleteTwo: null
-        }
+        body: { }
     };
 
-    const deleteOne = await db.delete(params, function(err, data) {
-        if (err) response.body.deleteOne = "DELETE-ONE-FAIL";
-        else response.body.deleteOne = "DELETE-ONE-PASS";
-    }).promise();
-
-    params.TableName = "id-room";
-
-    const deleteTwo = await db.delete(params, function(err, data) {
-        if (err) response.body.deleteTwo = "DELETE-TWO-FAIL";
-        else response.body.deleteTwo = "DELETE-TWO-PASS";
-    }).promise();
-
-    Promise.all([deleteOne, deleteTwo]);
+    await db.delete(params).promise()
+    .then(() => {
+        response.body.delete = "SUCCESS";
+    })
+    .catch((err) => {
+        response.body.delete = "FAIL";
+    });
     response.body = JSON.stringify(response.body);
     return response;
 };
