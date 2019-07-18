9e322e83af3d7d9c4811690b5ba4a97671008b9a
Christopher Hillbrand <cjhillbrand@gmail.com>
2019-07-18 12:39:11 -0400

    Hardcoded websocket url (#1)

--- chatroom/awsconfig.js
+++ chatroom/awsconfig.js
@@ -1,6 +1,6 @@
 const AWS_CONFIG = {
     "region": "us-east-1",
-    "websocket": "<Your WebSocket URL>",
+    "websocket": "unsafe:wss://v043rer1p1.execute-api.us-east-1.amazonaws.com/development",
     "name": "Websocket-Lab",
 }
 
